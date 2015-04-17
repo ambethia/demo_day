fd6f1392c0b0040c057172f18656e40ed5c98b3f
Jason L Perry <jasper@ambethia.com>
2015-04-17 00:45:05 -0400

    Column

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register Project do
-  permit_params :cohort_id, :program_id, :description, :title, :web_url, :repo_url
+  permit_params :cohort_id, :program_id, :description, :title, :web_url, :repo_url, :order
 
   index do
     selectable_column
     column :title
     column :cohort
     column :program
+    colomn :order
     actions
   end
 end
